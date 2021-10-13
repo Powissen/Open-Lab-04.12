f2bed16d34b7a2e89469277b757962c5d30c2191
Powissen <lukasloksa2@gmail.com>
2021-10-13 09:06:43 +0200

    Function that returns missing number from array

--- Open-Lab-04.12/Numbers.cs
+++ Open-Lab-04.12/Numbers.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._12
 {
     public class Numbers
     {
+        int output;
         public int MissingNum(int[] nums)
         {
-            throw new NotImplementedException();
+            for (int num = 0; num < 11; num++)
+            {
+            if (!nums.Contains(num))
+                {
+                    output = num;
+                }
+
+            }
+            return output;
         }
     }
 }
